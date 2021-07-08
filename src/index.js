fff2634e5db591041f7f1a570fde9b1800868b81
yukai pc <1079869931@qq.com>
2021-07-09 07:23:41 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import dva from 'dva';
+
+const createHistory = require('history').createBrowserHistory;
+
+const app = dva({
+  history: createHistory(),
+});
+
+// app.model(require('./models/home').default);
+
+app.router(require('./router').default);
+
+const APP = app.start();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <APP></APP>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 性能测试
+// reportWebVitals();
