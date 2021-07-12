1086bae31268946416e9123856ff65aa4167327a
yukai pc <1079869931@qq.com>
2021-07-12 23:49:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import Register from './pages/register/register';
+import Login from './pages/login/login';
+import Home from './pages/home/home';
 import './index.css';
-// import reportWebVitals from './reportWebVitals';
-import dva from 'dva';
-
-const createHistory = require('history').createBrowserHistory;
-
-const app = dva({
-  history: createHistory(),
-});
-
-// app.model(require('./models/home').default);
-
-app.router(require('./router').default);
-
-const APP = app.start();
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <APP></APP>
+    <HashRouter>
+      <Switch>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route component={Home}></Route>
+      </Switch>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// 性能测试
-// reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-/**
- * 服务代理
- */
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function (app) {
-    app.use(createProxyMiddleware('/HomeApp', { target: 'http://localhost:8088' }));
-};
