5bcf16c67b5d51a0f76c00374a4007dd5fda3305
yukai pc <1079869931@qq.com>
2021-09-29 22:46:53 +0800

    update

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,12 +1,38 @@
 /**
  * 个人中心
  */
-import React from 'react'
+import React from 'react';
+import './profile.less';
 
 export default function Profile() {
     return (
-        <div>
-            Profile
+        <div className="page-of-profile">
+            <div className="header">
+                <img src={require(`../../assets/images/logo.jpg`)}></img>
+                <span>18375183097</span>
+                <button>药师指导</button>
+            </div>
+            <div className="orders">
+                <p>我的订单</p>
+                <div className="container">
+                    <div className="item">
+                        <img src="" />
+                        <span>待付款</span>
+                    </div>
+                    <div className="item">
+                        <img src="" />
+                        <span>待发货</span>
+                    </div>
+                    <div className="item">
+                        <img src="" />
+                        <span>待收货</span>
+                    </div>
+                    <div className="item">
+                        <img src="" />
+                        <span>待评价</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
